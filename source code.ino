c01d5c9d21e2b2d5e3688eb922fdd2ebd6abb2c8
goteninwza007 <73677460+goteninwza007@users.noreply.github.com>
2021-05-16 20:36:08 +0700

    Add files via upload

---
+++ source code.ino
@@ -0,0 +1,125 @@
+
+/**
+ * Created by K. Suwatchai (Mobizt)
+ * 
+ * Email: k_suwatchai@hotmail.com
+ * 
+ * Github: https://github.com/mobizt
+ * 
+ * Copyright (c) 2021 mobizt
+ *
+*/
+
+#if defined(ESP32)
+#include <WiFi.h>
+#include <FirebaseESP32.h>
+#elif defined(ESP8266)
+#include <ESP8266WiFi.h>
+#include <FirebaseESP8266.h>
+#endif
+
+//Provide the token generation process info.
+#include "addons/TokenHelper.h"
+//Provide the RTDB payload printing info and other helper functions.
+#include "addons/RTDBHelper.h"
+
+/* 1. Define the WiFi credentials */
+#define WIFI_SSID "YouAreMyEverything"
+#define WIFI_PASSWORD "1111100000"
+
+/* 2. Define the API Key */
+#define API_KEY "AIzaSyCWt9DfgW7TocaM9OsCrHQn1ExOHWJEZKI"
+
+/* 3. Define the RTDB URL */
+#define DATABASE_URL "project-a0edb-default-rtdb.asia-southeast1.firebasedatabase.app" //<databaseName>.firebaseio.com or <databaseName>.<region>.firebasedatabase.app
+
+/* 4. Define the user Email and password that alreadey registerd or added in your project */
+#define USER_EMAIL "goten10_@hotmail.co.th"
+#define USER_PASSWORD "Goten12188"
+
+//Define Firebase Data object
+FirebaseData fbdo;
+
+FirebaseAuth auth;
+FirebaseConfig config;
+
+bool taskCompleted = false;
+
+FirebaseJson json;
+
+void setup()
+{
+  pinMode(D0,OUTPUT);
+  Serial.begin(115200);
+
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(300);
+  }
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+
+  /* Assign the api key (required) */
+  config.api_key = API_KEY;
+
+  /* Assign the user sign in credentials */
+  auth.user.email = USER_EMAIL;
+  auth.user.password = USER_PASSWORD;
+
+  /* Assign the RTDB URL (required) */
+  config.database_url = DATABASE_URL;
+
+  /* Assign the callback function for the long running token generation task */
+  config.token_status_callback = tokenStatusCallback; //see addons/TokenHelper.h
+
+  Firebase.begin(&config, &auth);
+
+  //Or use legacy authenticate method
+  //Firebase.begin(DATABASE_URL, DATABASE_SECRET);
+
+  Firebase.reconnectWiFi(true);
+
+#if defined(ESP8266)
+  //Set the size of WiFi rx/tx buffers in the case where we want to work with large data.
+  fbdo.setBSSLBufferSize(1024, 1024);
+#endif
+
+  //Set the size of HTTP response buffers in the case where we want to work with large data.
+  fbdo.setResponseSize(1024);
+
+  //Set database read timeout to 1 minute (max 15 minutes)
+  Firebase.setReadTimeout(fbdo, 1000 * 60);
+  //tiny, small, medium, large and unlimited.
+  //Size and its write timeout e.g. tiny (1s), small (10s), medium (30s) and large (60s).
+  Firebase.setwriteSizeLimit(fbdo, "tiny");
+
+  //optional, set the decimal places for float and double data to be stored in database
+  Firebase.setFloatDigits(2);
+  Firebase.setDoubleDigits(6);
+
+  /**
+   * This option allows get and delete functions (PUT and DELETE HTTP requests) works for device connected behind the
+   * Firewall that allows only GET and POST requests.
+   * 
+   * Firebase.enableClassicRequest(&fbdo, true);
+  */
+}
+
+void loop()
+{
+ if(Firebase.getInt(fbdo, "user/status")){
+  if(fbdo.dataType() == "int"){
+    Serial.println(fbdo.intData());
+    digitalWrite(D0,!(fbdo.intData()));
+  }
+  else{
+      Serial.println(fbdo.errorReason());
+  }
+ }
+ delay(500);
+}
